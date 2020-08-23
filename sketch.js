7cbc92ec99e088634ad8c70557e2f2b91978acd1
browny-2019 <shambhavi.vish@gmail.com>
2020-08-23 12:12:08 +0530

    homework

--- sketch.js
+++ sketch.js
@@ -1,11 +1,21 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+
 function setup() {
   createCanvas(800,400);
-  base1 = new Base(400,380,50,10);
+  engine = Engine.create();
+   world = engine.world;
+  base1 = new Base(400,380,200,10);
   
 }
 
 function draw() {
-  background(0,0,0);  
+  background(0,0,0); 
+  Engine.update(engine); 
   base1.display();
 
   drawSprites();
